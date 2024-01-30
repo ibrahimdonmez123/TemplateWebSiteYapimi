ab9d8417c2564c1da913fdfc24fd5e49367f2de5
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 23:44:08 +0300

    Add project files.

---
+++ TemplateWebSiteYapimi/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TemplateWebSiteYapimi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Slider()
+        {
+            return View();
+        }
+
+        public ActionResult Faq()
+        {
+            return View();
+        }
+
+        public ActionResult dortyuzdort()
+        {
+            return View();
+        }
+
+        public ActionResult sidebar()
+        {
+            return View();
+        }
+    }
+}
